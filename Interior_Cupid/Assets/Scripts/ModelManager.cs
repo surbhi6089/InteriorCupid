c5ad598dd8a70abf201209ae81ef874bae751b4e
surbhi6089 <66399968+surbhi6089@users.noreply.github.com>
2023-03-21 15:03:12 +0545

    Added marker/pointer feature for AR object placement

--- Interior_Cupid/Assets/Scripts/ModelManager.cs
+++ Interior_Cupid/Assets/Scripts/ModelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation; //has the class ARRaycastManager and other similar classes
 
 //this script manages the placement of models/objects into the real world
@@ -12,18 +13,50 @@ public class ModelManager : MonoBehaviour
     [SerializeField] private ARRaycastManager raycastManager;
 
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private Touch touch;
+    [SerializeField] private GameObject pointer;
+    private Pose pose;
 
     public void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        PointerCalculation();
+
+        touch = Input.GetTouch(0);
+
+        if(Input.touchCount<0 || touch.phase != TouchPhase.Began)
         {
-            //the camera reads the location of the mouse/touch position and throws a ray from that point to the AR scene, it converts the screen point to a ray
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            if (raycastManager.Raycast(ray, hits))
-            {
-                Pose pose = hits[0].pose;
-                Instantiate(DataCollector.Instance.model, pose.position, pose.rotation);
-            }
+            return;
         }
+        if (IsTouchOnUI(touch))
+        {
+            return ;
+        }
+        
+        Instantiate(DataCollector.Instance.model, pose.position, pose.rotation);
+
+    }
+
+    bool IsTouchOnUI(Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List<RaycastResult> result = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, result);
+        return result.Count > 0;
+    }
+
+    void PointerCalculation()
+    {
+        Vector3 origin = camera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
+
+        //the camera reads the location of the mouse/touch position and throws a ray from that point to the AR scene, it converts the screen point to a ray
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (raycastManager.Raycast(ray, hits))
+        {
+            pose = hits[0].pose;
+            pointer.transform.position = pose.position;
+            pointer.transform.eulerAngles = new Vector3(90, 0, 0);
+        }
+
     }
 }
